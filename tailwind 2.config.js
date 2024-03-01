d7e268f99a0de4432b215275d6395a3c6f662499
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2024-03-01 16:50:40 +0100

    test

---
+++ tailwind 2.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#621FFF",
+        black: "#0E131F",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
